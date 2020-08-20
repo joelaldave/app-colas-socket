c7926af996a889a3f3f0c87baa1015065cad6483
Joel Aldave <aldavecueva@gmail.com>
2020-08-19 21:25:44 -0400

    Primer commit Produccion

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Se perdio la coneccion al servidor');
+});
+
+socket.on('estadoActual', function( resp ) {
+    label.text( resp.actual );
+})
+
+$('button').on('click', function () {
+
+    socket.emit('siguienteTicket', null , function( SiguienteTicket ) {
+        
+        label.text(SiguienteTicket);
+
+    });
+
+    
+});
